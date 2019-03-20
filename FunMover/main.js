8c8a99083290214c032852530c0c935238979747
pmarlowe791321 <791321@seq.org>
2019-03-20 10:23:12 -0700

    progress

--- FunMover/main.js
+++ FunMover/main.js
@@ -3,27 +3,37 @@ var canvas;
 var ctx;
 var snakey;
 var lebron;
+var lebronLoc;
+var canvasLoc;
+var center;
 function init(){
   canvas = document.getElementById('cnv');
   ctx = canvas.getContext('2d');
-  lebron = document.getElementById('james');
+  var templebron = new Image();
+  templebron.src = 'image1.jpg';
+  templebron.addEventListener('load',function(e){lebron=this});
   canvas.width = 800;
   canvas.height = 600;
   canvas.style.border = 'solid black 1px';
   canvas.style.backgroundColor = 'rgba(12,15,25, .9)';
   document.addEventListener('keydown',keydownHandler);
   snakey = new Snake(200,200,20);
+  center = new JSVector(canvas.width/2,canvas.height/2);
   animate();
 }
 
 function animate(){
 requestAnimationFrame(animate);
+console.log(snakey.loc.x+' '+snakey.loc.y)
+//var dLoc = JSVector.subGetNew(mouseLoc,center);
+//dLoc.multiply(0.1);
+//canvasLoc.add(dLoc);
 ctx.clearRect(0,0,canvas.height,canvas.width);
-  snakey.run();
+snakey.run();
 }
 
 function keydownHandler(event){
-  console.log(event.key);
+  //console.log(event.key);
   if(event.key == 'w'){
     snakey.loc.y+=-7;
   }
--- FunMover/snake.js
+++ FunMover/snake.js
@@ -13,7 +13,9 @@ Snake.prototype.update = function(){
   this.loc.y+=this.vel.y;
 }
 Snake.prototype.render = function(){
+  if(lebron){
   ctx.drawImage(lebron,50, 70, 290, 300,this.loc.x,this.loc.y,100,100);
+  }
   // ctx.beginPath();
   // ctx.arc(this.loc.x,this.loc.y,this.rad,0, Math.PI*2, false)
   // ctx.fillStyle = "rgb(255,162,12)";
